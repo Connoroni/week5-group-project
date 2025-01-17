0573101ca5106e9dca422651fd95472028662fc1
Connoroni <mitchell2k2@gmail.com>
2025-01-17 09:13:32 +0000

    changed link to deployment url

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 const priceDiv = document.getElementById("price-div");
+priceDiv.style.display = "none";
 const driverDiv = document.getElementById("driver-div");
+driverDiv.style.display = "none";
 
 const locationForm = document.getElementById("flexfare");
 // console.log(locationForm);
@@ -9,13 +11,16 @@ locationForm.addEventListener("submit", async (event) => {
   const formData = new FormData(locationForm);
   const formObject = Object.fromEntries(formData);
   console.log("FORM OBJECT:", formObject);
-  const result = await fetch("http://localhost:8080/retrieve", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formObject),
-  });
+  const result = await fetch(
+    "https://week5-group-project-server.onrender.com/retrieve",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    }
+  );
   const jsonResult = await result.json();
   console.log("JSON RESULT:", jsonResult[0]);
   const calculateText = document.createElement("h1");
@@ -28,13 +33,15 @@ locationForm.addEventListener("submit", async (event) => {
 });
 
 function renderPrice() {
+  priceDiv.style.display = "flex";
   priceDiv.innerHTML = `<form id="priceForm"
   <label>Enter your offer here: £</label>
-  <input type="number" name="actual_price" id="actual_price_input" value="1000000000" placeholder="Enter your offer here"/> <button type="submit">Send offer</button> 
+  <input type="number" name="actual_price" id="actual_price_input" value="50" placeholder="Enter your offer here"/> <button type="submit">Send offer</button> 
   </form>`;
   const priceForm = document.getElementById("priceForm");
   priceForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    driverDiv.style.display = "flex";
     driverDiv.innerHTML = `
     <h2>3 Drivers Available</h2>
     <div class="driver">
